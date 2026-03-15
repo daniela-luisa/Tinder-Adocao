2d065c1397c08a9095ff20788299222ddae7931a
Daniela <danielaluisaconceicao9063@gmail.com>
2026-03-15 16:47:49 -0300

    create do Usuario OK

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,37 +1,9 @@
-/**
- * Route Mappings
- * (sails.config.routes)
- *
- * Your routes tell Sails what to do each time it receives a request.
- *
- * For more information on configuring custom routes, check out:
- * https://sailsjs.com/anatomy/config/routes-js
- */
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
-
-  '/': { view: 'pages/homepage' },
-
-
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
-
-
+  'POST /usuario/create' : { action: 'usuario/create'},
+  // 'DELETE /usuario/delete/:id' : { action: 'usuario/delete'},
+  // 'GET /usuario/readall': { action: 'usuario/readall'},
+  // 'GET /usuario/readoneid/:id': { action: 'usuario/readoneid'},
+  // 'PUT /usuario/update/:id': { action: 'usuario/update'},
 };
